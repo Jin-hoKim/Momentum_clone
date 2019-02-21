8be7bc56e37a2e7f4b0d1d62a604a8dd22cca7af
Jin-hoKim <wltn222@gmail.com>
2019-02-21 17:36:02 +0900

    added todo control

--- js/todolist.js
+++ js/todolist.js
@@ -4,19 +4,59 @@ const todoList = document.querySelector(".js-todoList");
 
 const TODO_LS = "toDos";
 
+let toDos = [];
+
+function filterFunc(todo)
+{
+
+}
+
+function deleteTodo(event)
+{
+  console.log(event.target.parentNode);
+  const btn = event.target;
+  const li = btn.parentNode;
+
+  todoList.removeChild( li );
+
+  const cleanTodos = toDos.filter( function(todo) {
+    return todo.id = parseInt(li.id);
+  });
+
+  toDos = cleanTodos;
+  saveTodos();
+}
+
+function saveTodos()
+{
+  localStorage.setItem(TODO_LS, JSON.stringify(toDos));
+}
+
 function paintTodo(txt)
 {
   const delBtn = document.createElement("button");
   delBtn.innerHTML = "X";
+  delBtn.addEventListener("click", deleteTodo);
 
   const span = document.createElement("span");
   span.innerText = txt;
 
+  const idx = toDos.length + 1;
+
   const li = document.createElement("li");
+  li.id = idx;
   li.appendChild(delBtn);
   li.appendChild(span);
 
   todoList.appendChild(li);
+
+  const todoObj = {
+    txt: txt,
+    id: idx
+  };
+
+  toDos.push( todoObj );
+  saveTodos();
 }
 
 function handleSubmit(event)
@@ -29,10 +69,14 @@ function handleSubmit(event)
 
 function loadTodoList()
 {
-  const todos = localStorage.getItem(TODO_LS);
-  if( todos  )
+  const loadedTodos = localStorage.getItem(TODO_LS);
+  if( loadedTodos  )
   {
-
+    const parsedTodos = JSON.parse(loadedTodos);
+    console.log(parsedTodos);
+    parsedTodos.forEach( function(todo) {
+      paintTodo( todo.txt );
+    });
   }
 }
 
